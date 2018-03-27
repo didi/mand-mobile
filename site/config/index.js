6ee3cc7b679e09d8b6c1282dac9f3e6d3866a020
zhaozhe <zhaozhe@didichuxing.com>
2018-03-27 16:48:28 +0800

    docs(site): adjust the asset paths of the doc site.

--- site/config/index.js
+++ site/config/index.js
@@ -9,8 +9,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../../docs'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: mbConfig.staticPrefix ? mbConfig.staticPrefix : '/',
+    assetsSubDirectory: 'mand-mobile/static',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
