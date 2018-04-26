999a35559cd9c7db901b0e289b706ae6c2117b3f
remote_star <148054685@qq.com>
2018-04-26 14:22:52 +0800

    doc(site): add code copy function.

--- site/theme/default/main.js
+++ site/theme/default/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueClipboard from 'vue-clipboard2'
 import { setScale } from './assets/js/util'
 
 Vue.config.productionTip = false
+Vue.use(VueClipboard)
 
 if ($(window).width() > 750) {
   setScale(0.5)
