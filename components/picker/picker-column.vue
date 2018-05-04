7ec841cc9e32e994728cf97dd1851ae7f21b52a2
moyus <moyuboy@gmail.com>
2018-05-04 20:06:54 +0800

    fix(picker): missing data reassignment

--- components/picker/picker-column.vue
+++ components/picker/picker-column.vue
@@ -52,7 +52,7 @@
 
 <script>import Scroller from '../_util/scroller'
 import {render} from '../_util/render'
-import {noop, getDpr, traverse, inArray, compareObjects, warn} from '../_util'
+import {noop, getDpr, traverse, inArray, warn} from '../_util'
 
 const dpr = getDpr()
 const API_LIST = [
@@ -120,18 +120,15 @@ export default {
 
   watch: {
     data: {
-      handler(val, oldVal) {
-        /* istanbul ignore if */
-        if (!compareObjects(val, oldVal)) {
-          this.columnValues = val
-        }
+      handler(val) {
+        this.columnValues = [...val]
       },
       deep: true,
     },
   },
 
   created() {
-    this.columnValues = this.data
+    this.columnValues = [...this.data]
   },
 
   methods: {
