b9a0746cf5a83eacd6f2d6ddee8fa3835f32f124
xuxiaoyan <xuxiaoyan@didichuxing.com>
2018-07-13 11:53:51 +0800

    site: update style & add version to header

--- site/theme/default/Preview.vue
+++ site/theme/default/Preview.vue
@@ -3,9 +3,9 @@
     <div class="preview-category">
       <div class="preview-category-title"><i class="icon-hollow-money"></i>Financial Components</div>
       <div class="preview-category-list">
-        <template v-for="category in components">
+        <template v-for="(category, i) in components">
           <template v-for="(component, j) in category.menu">
-            <div class="preview-category-item" :key="`financial-${j}`" v-if="~financialComponents.indexOf(component.name)">
+            <div class="preview-category-item" :key="`financial-${i}-${j}`" v-if="~financialComponents.indexOf(component.name)">
               <router-link :to="`/${lang}/docs/components/${category.name}/${component.name}`">
                 <div class="preview-category-item-img">
                   <img :src="`//manhattan.didistatic.com/static/manhattan/mand/preview/${component.name}.jpg`" :alt="component.name">
