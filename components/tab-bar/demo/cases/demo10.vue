06ce7551b4cfe719f7aad6b5f57f0d92edac9656
moyus <moyuboy@gmail.com>
2018-07-19 23:34:11 +0800

    feat(tab-bar): add scrollable support

---
+++ components/tab-bar/demo/cases/demo10.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="md-example-child md-example-child-tab-bar md-example-child-tab-bar-0">
+    <md-tab-bar :titles="titles"></md-tab-bar>
+  </div>
+</template>
+
+<script>import {TabBar} from 'mand-mobile'
+
+export default {
+  name: 'tab-bar-demo',
+  /* DELETE */
+  title: '多项滚动',
+  titleEnUS: 'Scrollable',
+  /* DELETE */
+  components: {
+    [TabBar.name]: TabBar,
+  },
+  data() {
+    return {
+      titles: ['第一', '第二', '第三', '第四', '第五', '第六', '第七', '第八', '第九'],
+    }
+  },
+}
+</script>
