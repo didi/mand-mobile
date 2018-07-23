1c2cea3355d40a5cea93306cea81469f9cc8753f
moyus <moyuboy@gmail.com>
2018-07-23 18:27:52 +0800

    fix: Improve type definitions #153

--- types/dialog.d.ts
+++ types/dialog.d.ts
@@ -8,20 +8,27 @@ export type DialogOptions = {
 }
 
 export type DialogAlertOptions = {
-  icon?: string
+  icon?: string,
+  closable?: boolean,
 } & DialogOptions
-
 
 export type DialogConfirmOptions = {
   cancelText?: string
 } & DialogAlertOptions
 
+export type DialogSucceedOptions = {
+  cancelText?: string,
+  closable?: boolean,
+} & DialogOptions
+
+export type DialogFailedOptions = DialogSucceedOptions
+
 export interface Dialog {
   confirm(options: DialogConfirmOptions): Vue
   alert(options: DialogAlertOptions): Vue
-  succeed(options: DialogOptions): Vue
-  failed(options: DialogOptions): Vue
-  close(): void
+  succeed(options: DialogSucceedOptions): Vue
+  failed(options: DialogFailedOptions): Vue
+  closeAll(): void
 }
 
 export const Dialog: Dialog
