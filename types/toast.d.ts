834d208dd778fc489ecce61d90acc0681aa0e90d
moyu <moyuboy@gmail.com>
2018-07-23 18:53:30 +0800

    fix: Improve type definitions #153 (#155)

--- types/toast.d.ts
+++ types/toast.d.ts
@@ -1,15 +1,18 @@
 export type ToastOptions = {
   content: string
   duration: number
-  parentNode: Element
+  parentNode: Element,
+  hasMask?: boolean
 }
 
 export type ToastConstructorOptions = {
-  icon: string
+  icon: string,
+  position?: string
 } & ToastOptions
 
 export interface Toast {
   (options?: ToastConstructorOptions): void
+  info(options?: ToastOptions): void
   succeed(options?: ToastOptions): void
   failed(options?: ToastOptions): void
   loading(options?: ToastOptions): void
