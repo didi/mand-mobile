22aa10e4c3f16981985fa79874f700352f72f002
supergaojian <402853745@qq.com>
2018-09-05 15:52:04 +0800

    feat(build): mand mobile 2.0 for mpvue/weapp add app.json

--- examples/weapp.main.js
+++ examples/weapp.main.js
@@ -6,11 +6,3 @@ App.mpType = 'app'
 
 const app = new Vue(App)
 app.$mount()
-
-export default {
-  // 这个字段走 app.json
-  config: {
-    // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['^/pages/list/list']
-  }
-}
--- examples/weapp/app.js
+++
@@ -1,4 +0,0 @@
-
-require('./static/js/manifest')
-require('./static/js/vendor')
-require('./static/js/app')
