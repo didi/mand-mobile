f5a873234a0a24a88dc46b862f10b82ce6c28430
xuxiaoyan <xuxiaoyan@didichuxing.com>
2018-09-18 14:19:04 +0800

    feat(switch): fit for weapp

--- components/switch/demo/index.vue
+++ components/switch/demo/index.vue
@@ -1,22 +1,49 @@
 <template>
   <div class="md-example switch">
-    <section class="md-example-section" v-for="(demo, index) in demos" :key="index">
-      <div class="md-example-title" v-html="demo.title || '基础'"></div>
-      <div class="md-example-describe" v-html="demo.describe"></div>
+    <section class="md-example-section">
+      <div class="md-example-title"></div>
+      <div class="md-example-describe"></div>
       <div class="md-example-content">
-        <component :is="demo"></component>
+        <demo0></demo0>
+      </div>
+    </section>
+    <section class="md-example-section">
+      <div class="md-example-title"></div>
+      <div class="md-example-describe"></div>
+      <div class="md-example-content">
+        <demo1></demo1>
+      </div>
+    </section>
+    <section class="md-example-section">
+      <div class="md-example-title"></div>
+      <div class="md-example-describe"></div>
+      <div class="md-example-content">
+        <demo2></demo2>
+      </div>
+    </section>
+    <section class="md-example-section">
+      <div class="md-example-title"></div>
+      <div class="md-example-describe"></div>
+      <div class="md-example-content">
+        <demo3></demo3>
       </div>
     </section>
 	</div>
 </template>
 
-<script>import createDemoModule from '../../../examples/create-demo-module'
-import Demo0 from './cases/demo0'
+<script>import Demo0 from './cases/demo0'
 import Demo1 from './cases/demo1'
 import Demo2 from './cases/demo2'
 import Demo3 from './cases/demo3'
 
-export default {...createDemoModule('switch', [Demo0, Demo1, Demo2, Demo3])}
+export default {
+  components: {
+    Demo0,
+    Demo1,
+    Demo2,
+    Demo3,
+  },
+}
 </script>
 
 <style lang="stylus" scoped>
--- examples/weapp/main.js
+++ examples/weapp/main.js
@@ -4,6 +4,9 @@ import Weapp from '../../platforms/weapp'
 
 import Button from '../../components/button'
 import ActionBar from '../../components/action-bar'
+import Field from '../../components/field'
+import FieldItem from '../../components/field/item'
+import Switch from '../../components/switch'
 
 Vue.config.productionTip = false
 App.mpType = 'app'
@@ -11,6 +14,9 @@ App.mpType = 'app'
 Vue.use(Weapp)
 Vue.component('mdButton', Button)
 Vue.component('mdActionBar', ActionBar)
+Vue.component('mdField', Field)
+Vue.component('mdFieldItem', FieldItem)
+Vue.component('mdSwitch', Switch)
 
 const app = new Vue(App)
 app.$mount()
