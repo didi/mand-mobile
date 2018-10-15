70d8eba77ed41521c37680e2420a0d94dc978be5
lucker <guoyunlong@didichuxing.com>
2018-10-15 19:32:19 +0800

    fix(test): add jest snapshot

--- components/tag/jest-test/index.spec.js
+++ components/tag/jest-test/index.spec.js
@@ -4,6 +4,6 @@ import Component from '../index'
 describe('Component', () => {
   test('是一个 Vue 实例', () => {
     const wrapper = mount(Component)
-    expect(wrapper.isVueInstance()).toBeTruthy()
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
