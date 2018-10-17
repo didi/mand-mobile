ad8d71a7bbe210fe602d54288b6f16ce80ac0297
moyu <moyuboy@gmail.com>
2018-10-17 23:02:47 +0800

    fix(button): click event not trigger in some old browsers (#214)

--- components/button/index.vue
+++ components/button/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div
+  <button
+    type="button"
     class="md-button"
-    :class="[type, size, disabled ? 'disabled' : '', icon ? 'with-icon' : '']"
-    @click="$_onBtnClick"
+    :class="[type, size, icon ? 'with-icon' : '']"
+    :disabled="disabled"
+    v-on="$listeners"
   >
     <div class="md-button-inner">
-      <template v-if="icon">
-        <md-icon :name="icon"></md-icon>
-      </template>
+      <md-icon v-if="icon" :name="icon"></md-icon>
       <slot></slot>
     </div>
-  </div>
+  </button>
 </template>
 
 <script>import Icon from '../icon'
@@ -39,29 +39,25 @@ export default {
       default: false,
     },
   },
-
-  methods: {
-    $_onBtnClick(event) {
-      if (this.disabled) {
-        event.stopImmediatePropagation()
-      } else {
-        this.$emit('click', event)
-      }
-    },
-  },
 }
 </script>
 
 <style lang="stylus">
 .md-button
+  display block
   -webkit-user-select none
   -webkit-tap-highlight-color transparent
   position relative
   text-align center
   border-radius button-primary-radius
+  background none
+  border none
+  box-shadow none
+  outline none
+  appearance none
   box-sizing border-box
   overflow visible
-  &.disabled
+  &:disabled
     &:active::before
       display none
   &::before
@@ -89,7 +85,7 @@ export default {
     color color-text-base-inverse
     &:active::before
       background-color button-primary-fill-tap
-    &.disabled
+    &:disabled
       background-color button-primary-fill-disabled
     &.large, &.small
       width button-primary-width
@@ -109,7 +105,7 @@ export default {
     &:active::before
       background-color button-ghost-primary-fill-tap
   &.ghost, &.ghost-primary
-    &.disabled
+    &:disabled
       opacity button-disabled-opacity
     &.large
       width button-ghost-width
@@ -130,7 +126,7 @@ export default {
       hairline(bottom, button-link-border)
     &:active::before
       background-color button-link-fill-tap
-    &.disabled
+    &:disabled
       opacity button-disabled-opacity
     &.large, &.small
       width button-link-width
