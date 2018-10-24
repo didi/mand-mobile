864be038d6a43a979d107a1565a24f93a5974b0a
xuxiaoyan <xuxiaoyan@didichuxing.com>
2018-10-24 20:43:05 +0800

    build: remove css-variable build

--- build/stylus-mixin.js
+++ build/stylus-mixin.js
@@ -2,18 +2,24 @@ const path = require('path')
 const isSpecial = process.env.BUILD_TYPE === 'special'
 
 module.exports = function useMixin(style) {
-  if (isSpecial) {
-    return style
-      .import(path.join(__dirname, '../components/_style/mixin/theme.variable.styl'))
-      .import(path.join(__dirname, '../components/_style/mixin/util.styl'))
-      .import(path.join(__dirname, '../node_modules/nib/lib/nib/vendor'))
-      .import(path.join(__dirname, '../node_modules/nib/lib/nib/gradients'))
-  } else {
-    return style
-      .import(path.join(__dirname, '../components/_style/mixin/theme.components.styl'))
-      .import(path.join(__dirname, '../components/_style/mixin/theme.basic.styl'))
-      .import(path.join(__dirname, '../components/_style/mixin/util.styl'))
-      .import(path.join(__dirname, '../node_modules/nib/lib/nib/vendor'))
-      .import(path.join(__dirname, '../node_modules/nib/lib/nib/gradients'))
-  }
+  // if (isSpecial) {
+  //   return style
+  //     .import(path.join(__dirname, '../components/_style/mixin/theme.variable.styl'))
+  //     .import(path.join(__dirname, '../components/_style/mixin/util.styl'))
+  //     .import(path.join(__dirname, '../node_modules/nib/lib/nib/vendor'))
+  //     .import(path.join(__dirname, '../node_modules/nib/lib/nib/gradients'))
+  // } else {
+  //   return style
+  //     .import(path.join(__dirname, '../components/_style/mixin/theme.components.styl'))
+  //     .import(path.join(__dirname, '../components/_style/mixin/theme.basic.styl'))
+  //     .import(path.join(__dirname, '../components/_style/mixin/util.styl'))
+  //     .import(path.join(__dirname, '../node_modules/nib/lib/nib/vendor'))
+  //     .import(path.join(__dirname, '../node_modules/nib/lib/nib/gradients'))
+  // }
+  return style
+    .import(path.join(__dirname, '../components/_style/mixin/theme.components.styl'))
+    .import(path.join(__dirname, '../components/_style/mixin/theme.basic.styl'))
+    .import(path.join(__dirname, '../components/_style/mixin/util.styl'))
+    .import(path.join(__dirname, '../node_modules/nib/lib/nib/vendor'))
+    .import(path.join(__dirname, '../node_modules/nib/lib/nib/gradients'))
 }
