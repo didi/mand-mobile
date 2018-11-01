cf99afba09e1d39e4e2decf5cb16498fed3dd0c3
xuxiaoyan <xuxiaoyan@didichuxing.com>
2018-11-01 15:42:28 +0800

    example: responsive initialize synchronization

--- examples/assets/responsive.js
+++ examples/assets/responsive.js
@@ -1,26 +1,17 @@
 (function (window, document) {
-
-    function resize(){
-      var ww = window.innerWidth;
-      if (ww > window.screen.width) {
-        window.requestAnimationFrame(resize);
+  function resize () {
+    var ww = window.innerWidth
+    if (ww > window.screen.width) {
+      window.requestAnimationFrame(resize)
+    } else {
+      if (ww > 750) {
+        ww = 750
       }
-      else{
-        if (ww > 720) {
-          ww = 720
-        }
-        document.documentElement.style.fontSize = ww * 0.13333333333333333 + 'px';
-        document.body.style.opacity = 1;
-      }
+      document.documentElement.style.fontSize = ww * 100 / 750 + 'px'
     }
+  }
 
-    if (document.readyState !== 'loading') {
-      resize();
-    }
-    else {
-      document.addEventListener('DOMContentLoaded', resize);
-    }
+  resize()
 
-    window.addEventListener('resize', resize);
-
-  })(window, document)
+  window.addEventListener('resize', resize)
+})(window, document)
