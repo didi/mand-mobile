345683ba5db60ad0c6106f3d8825aae52f029fdc
xuxiaoyan <xuxiaoyan@didiglobal.com>
2018-11-23 10:49:54 +0800

    fix(button): bold border & fix line-height

--- components/button/index.vue
+++ components/button/index.vue
@@ -121,47 +121,47 @@ export default {
   &.default
     background-color button-default-fill
     color button-default-color
-    hairline(all, color-border-element, button-radius)
+    hairline(all, color-border-element, button-radius, 4px)
     &.active:active
       background-color button-default-active-fill
   &.primary
     background-color button-primary-fill
     color button-primary-color
-    hairline(all, button-primary-fill, button-radius)
+    hairline(all, button-primary-fill, button-radius, 4px)
     &.active:active
       background-color button-primary-active-fill
   &.warning
     background-color button-warning-fill
     color button-warning-color
-    hairline(all, button-warning-fill, button-radius)
+    hairline(all, button-warning-fill, button-radius, 4px)
     &.active:active
       background-color button-warning-active-fill
   &.disabled
     background-color button-disabled-fill
     color button-disabled-color
-    hairline(all, button-disabled-fill, button-radius)
+    hairline(all, button-disabled-fill, button-radius, 4px)
 
   &.plain
     background transparent
 
     &.default
       color button-default-color
-      hairline(all, color-border-element, button-radius)
+      hairline(all, color-border-element, button-radius, 4px)
       &.active:active
         background-color button-default-plain-active-fill
     &.primary
       color button-primary-fill
-      hairline(all, button-primary-fill, button-radius)
+      hairline(all, button-primary-fill, button-radius, 4px)
       &.active:active
         background-color button-primary-plain-active-fill
     &.warning
       color button-warning-fill
-      hairline(all, button-warning-fill, button-radius)
+      hairline(all, button-warning-fill, button-radius, 4px)
       &.active:active
         background-color button-warning-plain-active-fill
     &.disabled
       color button-disabled-fill
-      hairline(all, button-disabled-fill, button-radius)
+      hairline(all, button-disabled-fill, button-radius, 4px)
 
   &.round
     border-radius button-height
@@ -176,6 +176,7 @@ export default {
 
   &.small
     height button-small-height
+    line-height button-small-height
     font-size button-small-font-size
     &.round
       border-radius button-small-height
