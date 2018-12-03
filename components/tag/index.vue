40e06cec68de1b086a73bb646d6d0cee63db98aa
xuxiaoyan <xuxiaoyan@didiglobal.com>
2018-12-03 21:29:47 +0800

    fix(tag): update style

--- components/tag/index.vue
+++ components/tag/index.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="md-tag">
-    <div :class="computedClass" :style="[computedStyle, jsComputedStyle]">
+    <div :class="computedClass" :style="[computedStyle, jsStyle]">
       <template v-if="shape === 'quarter'">
-        <div class="quarter-wrap">
+        <div class="quarter-content">
           <slot></slot>
         </div>
+        <div class="quarter-bg" :style="computedStyle"></div>
       </template>
       <template v-else>
-        <div
-          class="left-coupon"
-          :style="{ background: fillColor ? 'radial-gradient(circle at left, transparent 33%, ' + fillColor + ' 33%)' : ''}"
-          v-if="shape === 'coupon'"
-        >
-        </div>
-        <slot></slot>
-        <div
-          class="right-coupon"
-          :style="{ background: fillColor ? 'radial-gradient(circle at right, transparent 33%, ' + fillColor + ' 33%)' : ''}"
-          v-if="shape === 'coupon'"
-        >
+        <div class="coupon-container" :style="computedStyle">
+          <div
+            class="left-coupon"
+            :style="{ background: fillColor ? 'radial-gradient(circle at left, transparent 33%, ' + fillColor + ' 33%)' : ''}"
+            v-if="shape === 'coupon'"
+          >
+          </div>
+          <slot></slot>
+          <div
+            class="right-coupon"
+            :style="{ background: fillColor ? 'radial-gradient(circle at right, transparent 33%, ' + fillColor + ' 33%)' : ''}"
+            v-if="shape === 'coupon'"
+          >
+          </div>
         </div>
       </template>
     </div>
@@ -58,7 +61,7 @@
   },
   data() {
     return {
-      jsComputedStyle: {},
+      jsStyle: {},
     }
   },
   computed: {
@@ -87,18 +90,19 @@
     },
   },
   mounted() {
-    const padding = Math.max(this.$el.offsetHeight * 0.15, 3)
-    this.$set(this.jsComputedStyle, 'padding', padding + 'px')
+    // const padding = Math.max(this.$el.offsetHeight * 0.15, 3)
+    // this.$set(this.jsStyle, 'paddingTop', padding + 'px')
+    // this.$set(this.jsStyle, 'paddingBottom', padding + 'px')
     this.$nextTick(() => {
       if (this.shape === 'circle') {
         const height = this.$el.offsetHeight / 2
-        this.$set(this.jsComputedStyle, 'paddingLeft', height + 'px')
-        this.$set(this.jsComputedStyle, 'paddingRight', height + 'px')
-        this.$set(this.jsComputedStyle, 'borderRadius', height + 'px')
+        this.$set(this.jsStyle, 'paddingLeft', height + 'px')
+        this.$set(this.jsStyle, 'paddingRight', height + 'px')
+        this.$set(this.jsStyle, 'borderRadius', height + 'px')
       }
-      if (this.shape === 'quarter') {
-        this.$set(this.jsComputedStyle, 'padding', '0 0 100% 0')
-      }
+      // if (this.shape === 'quarter') {
+      //   this.$set(this.jsStyle, 'padding', '0 0 100% 0')
+      // }
     })
   },
 }
@@ -115,18 +119,54 @@
     color tag-color
     border-color tag-color
   .shape-square
+    padding 0 12px
     border-radius 0
   .shape-fillet
+    padding 2px 8px
     border-radius tag-fillet-radius
   .shape-quarter
+    position relative
     display flex
-    height 0
-    border-radius 0 0 0 100%
+    width 86px
+    height 86px
+    background transparent !important
+    overflow hidden
+    .quarter-content
+      position relative
+      display flex
+      justify-content center
+      flex 1
+      z-index 2
+      padding-left 10%
+      padding-top 24%
+      box-sizing border-box
+    .quarter-bg
+      position absolute
+      top -100%
+      width 200%
+      height 200%
+      border-radius radius-circle
     .quarter-wrap
       display inline-block
-      padding 14px 10px 10px 14px
+      padding 16px 12px 10px 26px
+    .quarter-wrap-hidden
+      visibility hidden
+      display inline-block
+      padding 16px 12px 10px 26px
+    
+    &.size-small
+      width 66px
+      height 66px
+    &.size-tiny
+      width 46px
+      height 46px
+
   .shape-coupon
     position relative
+    padding 0 10px
+    background transparent !important
+    .coupon-container
+      padding 2px 0
     .left-coupon,
     .right-coupon
       position absolute
@@ -134,19 +174,27 @@
       width 10px
       height 100%
     .left-coupon
-      left -10px
+      left 0
     .right-coupon
-      right -10px
-  .shape-circle
+      right 0
+    
+    &.size-small
+      padding 0 8px
+      .left-coupon,
+      .right-coupon
+        width 8px
+    &.size-tiny
+      padding 0 5px
+      .left-coupon,
+      .right-coupon
+        width 5px
+
   .size-large
     font-size tag-large-font-size
-    padding 5px
   .size-small
     font-size tag-small-font-size
-    padding 5px
   .size-tiny
     font-size tag-tiny-font-size
-    padding 5px
   .type-fill
     color color-text-base-inverse
     background tag-color
