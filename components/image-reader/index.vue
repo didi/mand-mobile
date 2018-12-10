79187b1d359c7166f2d1cd7b9425afd217766e62
linkFly <linkFly6@live.com>
2018-12-10 15:53:19 +0800

    fix(image-reader): update style
    update image-reader input style

--- components/image-reader/index.vue
+++ components/image-reader/index.vue
@@ -180,6 +180,8 @@ export default {
   width 100%
   height 100%
   z-index 100
+  left 0
+  top 0
   -webkit-tap-highlight-color rgba(0,0,0,0)
 
 .md-image-reader-file
@@ -187,5 +189,7 @@ export default {
   width 100%
   height 100%
   opacity 0
+  left 0
+  top 0
   -webkit-tap-highlight-color rgba(0,0,0,0)
 </style>
