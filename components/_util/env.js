081363da1be3488102183e5ed1c3a49a2e448808
supergaojian <402853745@qq.com>
2018-12-10 09:53:51 +0800

    feat(_util): add judge ssr env condition

--- components/_util/env.js
+++ components/_util/env.js
@@ -1,8 +1,10 @@
+import Vue from 'vue'
+
 // Development environment
 export const isProd = process.env.NODE_ENV === 'production'
 
 // Browser environment sniffing
-export const inBrowser = typeof window !== 'undefined'
+export const inBrowser = Vue.prototype.$isServer || typeof window !== 'undefined'
 export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
 export const isAndroid = UA && UA.indexOf('android') > 0
 export const isIOS = UA && /iphone|ipad|ipod|ios/.test(UA)
