f3e367473c28a2adde78512a7186fdea18359e98
supergaojian <402853745@qq.com>
2018-12-10 16:28:23 +0800

    fix(_util): fix wrong condition for judge ssr env

--- components/_util/env.js
+++ components/_util/env.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 export const isProd = process.env.NODE_ENV === 'production'
 
 // Browser environment sniffing
-export const inBrowser = Vue.prototype.$isServer || typeof window !== 'undefined'
+export const inBrowser = !Vue.prototype.$isServer || typeof window !== 'undefined'
 export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
 export const isAndroid = UA && UA.indexOf('android') > 0
 export const isIOS = UA && /iphone|ipad|ipod|ios/.test(UA)
