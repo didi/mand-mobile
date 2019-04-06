be5a2d496ed9e0c747d1e4e7b26eab5808e528b5
xuxiaoyan <xuxiaoyan@didichuxing.com>
2019-04-06 22:38:44 +0800

    doc(ruler):update readme & meta info

--- components/ruler/component.js
+++ components/ruler/component.js
@@ -1,7 +1,7 @@
 export default {
-  "name": "ruler",
-  "text": "刻度尺",
-  "category": "form",
-  "description": "刻度尺",
-  "author": "supergaojian <402853745@qq.com>"
+  'name': 'ruler',
+  'text': '刻度尺',
+  'category': 'business',
+  'description': '刻度尺',
+  'author': 'supergaojian <402853745@qq.com>'
 }
