5ab446c3d4737cf0390dedd33857156827755bc1
supergaojian <402853745@qq.com>
2019-04-19 10:44:24 +0800

    feat(ruler,water-mark): change canvas font-family

--- components/ruler/index.vue
+++ components/ruler/index.vue
@@ -216,7 +216,7 @@ export default {
 
       ctx.lineWidth = 2
       ctx.font = `${_fontSize *
-        ratio}px Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,微软雅黑,Arial,sans-serif`
+        ratio}px DIN Alternate, "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif`
 
       for (let i = 0; i <= unitCount; i++) {
         const _x = x + i * blank
