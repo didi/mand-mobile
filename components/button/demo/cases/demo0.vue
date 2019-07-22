381cce1df4fd6407ce96632f477cb817129285ff
xuxiaoyan <xuxiaoyan@didiglobal.com>
2019-07-22 14:16:16 +0800

    example(button): add loading cases

--- components/button/demo/cases/demo0.vue
+++ components/button/demo/cases/demo0.vue
@@ -4,9 +4,10 @@
     <md-button type="default" inactive>Default Inactive</md-button>
     <md-button type="primary">Primary</md-button>
     <md-button type="primary" inactive>Primary Inactive</md-button>
+    <md-button type="primary" loading>Loading</md-button>         
     <md-button type="warning">Warning</md-button>
     <md-button type="warning" inactive>Warning Inactive</md-button>
-    <md-button type="disabled">Disabled</md-button>    
+    <md-button type="disabled">Disabled</md-button>
   </div>
 </template>
 
