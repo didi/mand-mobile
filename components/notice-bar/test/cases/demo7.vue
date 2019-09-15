de0372c7289f9a38274a39f2612499f9f1db4056
jian.gao01 <jian.gao01@liulishuo.com>
2019-09-16 03:01:12 +0800

    test(notice-bar): update notice-bar test demo
    feat #564

--- components/notice-bar/test/cases/demo7.vue
+++ components/notice-bar/test/cases/demo7.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="md-example-child md-example-child-notice-bar md-example-child-notice-bar-7">
-    <md-notice-bar>
-      <md-icon slot="left" class="md-notice-demo-icon md-notice-demo-icon-left" name="security"></md-icon>
-      为了确保您的资金安全，请设置支付密码
+    <md-notice-bar
+      mode="closable"
+      icon="volumn"
+      auto-scroll
+    >
+      为了确保您的资金安全，请设置支付密码。为了确保您的资金安全，请设置支付密码。为了确保您的资金安全，请设置支付密码。
+    </md-notice-bar>
+    <br />
+    <md-notice-bar
+      mode="closable"
+      icon="volumn"
+      auto-scroll
+      :animation-duration="8000"
+    >
+      请设置支付密码
     </md-notice-bar>
   </div>
 </template>
 
-<script>import {NoticeBar, Icon} from 'mand-mobile'
+<script>import {NoticeBar} from 'mand-mobile'
 
 export default {
   name: 'notice-bar-demo',
+  /* DELETE */
+  title: '自动滚动播放(不关心内容是否超出)',
+  titleEnUS: 'Scroll play',
+  background: '#fff',
+  /* DELETE */
   components: {
     [NoticeBar.name]: NoticeBar,
-    [Icon.name]: Icon,
   },
 }
 </script>
---
+++ components/notice-bar/test/cases/demo8.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="md-example-child md-example-child-notice-bar md-example-child-notice-bar-7">
+    <md-notice-bar>
+      <md-icon slot="left" class="md-notice-demo-icon md-notice-demo-icon-left" name="security"></md-icon>
+      为了确保您的资金安全，请设置支付密码
+    </md-notice-bar>
+  </div>
+</template>
+
+<script>import {NoticeBar, Icon} from 'mand-mobile'
+
+export default {
+  name: 'notice-bar-demo',
+  components: {
+    [NoticeBar.name]: NoticeBar,
+    [Icon.name]: Icon,
+  },
+}
+</script>
