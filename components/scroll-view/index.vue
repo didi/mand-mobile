4a451ac873278c44381627a47927e635f4106770
supergaojian <402853745@qq.com>
2020-02-04 14:12:21 +0800

    fix(scroll-view): change the refresh active event trigger timing (#643)
    fix #642

--- components/scroll-view/index.vue
+++ components/scroll-view/index.vue
@@ -186,11 +186,11 @@ export default {
           () => {
             this.isRefreshActive = true
             this.isRefreshing = false
+            this.$emit('refreshActive')
           },
           () => {
             this.isRefreshActive = false
             this.isRefreshing = false
-            this.$emit('refreshActive')
           },
           () => {
             this.isRefreshActive = false
