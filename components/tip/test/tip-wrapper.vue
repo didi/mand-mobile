e7ecca6fee66a4816b1d840eef817583e9623e21
wukang0718 <42261663+wukang0718@users.noreply.github.com>
2020-12-08 19:41:21 +0800

    fix(tip): tip组件子组件是没有属性的原生标签时会报错 (#711)

--- components/tip/test/tip-wrapper.vue
+++ components/tip/test/tip-wrapper.vue
@@ -2,18 +2,18 @@
   <div class="wrapper0">
     <div class="wrapper1" scroll-wrapper>
       <md-tip content="不错哟" placement="top" ref="tip">
-        <md-button type="default">点击我</md-button>
+        <span>点击我</span>
       </md-tip>
     </div>
   </div>
 </template>
 
-<script>import Button from '../../button'
+<script>
 import Tip from '../index'
 export default {
   components: {
-    [Tip.name]: Tip,
-    [Button.name]: Button,
+    [Tip.name]: Tip
   },
 }
-</script>>  
+
+</script>
