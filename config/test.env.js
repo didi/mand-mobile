e431e5aa32280b29d846276a2ec12a6f4714eb3b
zouhang <zouhang@didiglobal.com>
2021-02-04 16:50:17 +0800

    fix(build): replace NODE_ENV with MAND_ENV in some components

--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  MAND_ENV: '"testing"',
 })
