59b43a3bb6e102f65fee0faad37aa74ed5903f40
MeloHG <2580757+qifeng0748@users.noreply.github.com>
2022-08-09 19:47:52 +0800

    feat(Toast): 增加 方形Toast多行文本 dome

--- components/toast/demo/index.vue
+++ components/toast/demo/index.vue
@@ -20,6 +20,7 @@ import Demo5 from './cases/demo5'
 import Demo6 from './cases/demo6'
 import Demo7 from './cases/demo7'
 import Demo8 from './cases/demo8'
+import Demo9 from './cases/demo9'
 
-export default {...createDemoModule('toast', [Demo0, Demo1, Demo2, Demo3, Demo4, Demo5, Demo6, Demo7, Demo8])}
+export default {...createDemoModule('toast', [Demo0, Demo1, Demo2, Demo3, Demo4, Demo5, Demo6, Demo7, Demo8, Demo9])}
 </script>
