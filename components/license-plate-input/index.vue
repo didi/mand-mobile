4ca061b5483ecae20079871caa8b66d6545651d3
MeloHG <2580757+qifeng0748@users.noreply.github.com>
2024-03-12 16:45:18 +0800

    style(LicensePlateInput): 修改车牌号字体

--- components/license-plate-input/index.vue
+++ components/license-plate-input/index.vue
@@ -72,7 +72,7 @@
     &_content {
       width: 100%;
       height: 100%;
-      font-family: font-family-number;
+      font-family: font-family-normal;
       font-size: 40px;
       color: #111A34;
       letter-spacing: 0;
